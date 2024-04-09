86a16eadabde4bd4c31e72da554592be2c9b4007
elma narimani <elma@dotical.org>
2024-04-09 06:38:08 +0330

    create custom inputs and using formik component

--- src/components/AdvancedForm.js
+++ src/components/AdvancedForm.js
@@ -1,9 +1,50 @@
+import { Formik, Form, Field } from "formik";
+import CustomInput from "./CustomInput";
+import { advancedSchema } from "../schemas";
+import CustomSelect from "./CustomSelect";
+import CustomCheckbox from "./CustomCheckbox";
+
+const onSubmit = async (values, actions) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  actions.resetForm();
+};
+
 const AdvancedForm = () => {
   return (
-    <form autoComplete="off">
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" placeholder="Enter your username" />
-    </form>
+    <Formik
+      initialValues={{ username: "", jobType: "", acceptedTos: false }}
+      validationSchema={advancedSchema}
+      onSubmit={onSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <CustomInput
+            label="Username"
+            name="username"
+            type="text"
+            placeholder="Enter your username"
+          />
+          <CustomSelect
+            label="Job type"
+            name="jobType"
+            type="select"
+            placeholder="Please select a job"
+          >
+            <option value="">Please select a job type</option>
+            <option value="developer">Developer</option>
+            <option value="designer">Designer</option>
+            <option value="manager">Product Manager</option>
+            <option value="other">Other</option>
+          </CustomSelect>
+          <CustomCheckbox name="acceptedTos" type="checkbox" />
+
+          <button disabled={isSubmitting} type="submit">
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 export default AdvancedForm;
